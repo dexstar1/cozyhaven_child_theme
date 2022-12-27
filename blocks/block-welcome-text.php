e9773c46853f7e0e104eeb2d7122d27ed1d6e8d0
jt8845186 <98743057+jt8845186@users.noreply.github.com>
2022-12-27 21:31:55 +0100

    added hero elements with styles

--- blocks/block-welcome-text.php
+++ blocks/block-welcome-text.php
@@ -1,2 +1,233 @@
-<p><?php block_field('welcometext'); ?></p>
+<p></p>
 <button href=""><?php block_field('button-field'); ?></button>
+
+
+
+<section class="elementor-section elementor-top-section elementor-element elementor-element-00ce695 elementor-section-height-min-height elementor-section-content-bottom elementor-section-boxed elementor-section-height-default elementor-section-items-middle" data-id="00ce695" data-element_type="section" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+							<div class="elementor-background-overlay"></div>
+							<div class="elementor-container elementor-column-gap-wide">
+							<div class="elementor-row">
+					<div class="elementor-column elementor-col-33 elementor-top-column elementor-element elementor-element-7448be2" data-id="7448be2" data-element_type="column">
+			<div class="elementor-column-wrap elementor-element-populated">
+							<div class="elementor-widget-wrap">
+						<div class="elementor-element elementor-element-ba3caf9 elementor-widget elementor-widget-houzez_elementor_section_title" data-id="ba3caf9" data-element_type="widget" data-widget_type="houzez_elementor_section_title.default">
+				<div class="elementor-widget-container">
+			            <div class="houzez_section_title_wrap section-title-module">
+                                    <h2 class="houzez_section_title">Why Cozy Havens is the Perfect Choice?</h2>
+                
+                            </div>
+            		</div>
+				</div>
+				<div class="elementor-element elementor-element-5388533 elementor-widget-divider--view-line elementor-widget elementor-widget-divider" data-id="5388533" data-element_type="widget" data-widget_type="divider.default">
+				<div class="elementor-widget-container">
+					<div class="elementor-divider">
+			<span class="elementor-divider-separator">
+						</span>
+		</div>
+				</div>
+				</div>
+				<div class="elementor-element elementor-element-7cef7f2 elementor-widget elementor-widget-heading" data-id="7cef7f2" data-element_type="widget" data-widget_type="heading.default">
+				<div class="elementor-widget-container">
+			<h2 class="elementor-heading-title elementor-size-default">01.</h2>		</div>
+				</div>
+				<div class="elementor-element elementor-element-d03729f elementor-widget elementor-widget-houzez_elementor_section_title" data-id="d03729f" data-element_type="widget" data-widget_type="houzez_elementor_section_title.default">
+				<div class="elementor-widget-container">
+			            <div class="houzez_section_title_wrap section-title-module">
+                                    <h2 class="houzez_section_title">Perfect Designation for property finders and investors</h2>
+                
+                            </div>
+            		</div>
+				</div>
+				<div class="elementor-element elementor-element-769da3b elementor-widget-divider--view-line elementor-widget elementor-widget-divider" data-id="769da3b" data-element_type="widget" data-widget_type="divider.default">
+				<div class="elementor-widget-container">
+					<div class="elementor-divider">
+			<span class="elementor-divider-separator">
+						</span>
+		</div>
+				</div>
+				</div>
+						</div>
+					</div>
+		</div>
+				<div class="elementor-column elementor-col-33 elementor-top-column elementor-element elementor-element-fd6f1cb" data-id="fd6f1cb" data-element_type="column">
+			<div class="elementor-column-wrap elementor-element-populated">
+							<div class="elementor-widget-wrap">
+						<div class="elementor-element elementor-element-c0294e9 elementor-widget elementor-widget-heading" data-id="c0294e9" data-element_type="widget" data-widget_type="heading.default">
+				<div class="elementor-widget-container">
+			<h2 class="elementor-heading-title elementor-size-default">02.</h2>		</div>
+				</div>
+				<div class="elementor-element elementor-element-1c55784 elementor-widget elementor-widget-houzez_elementor_section_title" data-id="1c55784" data-element_type="widget" data-widget_type="houzez_elementor_section_title.default">
+				<div class="elementor-widget-container">
+			            <div class="houzez_section_title_wrap section-title-module">
+                                    <h2 class="houzez_section_title">You get the most value in every dime </h2>
+                
+                            </div>
+            		</div>
+				</div>
+				<div class="elementor-element elementor-element-eadccc6 elementor-widget-divider--view-line elementor-widget elementor-widget-divider" data-id="eadccc6" data-element_type="widget" data-widget_type="divider.default">
+				<div class="elementor-widget-container">
+					<div class="elementor-divider">
+			<span class="elementor-divider-separator">
+						</span>
+		</div>
+				</div>
+				</div>
+				<div class="elementor-element elementor-element-8652a2e elementor-widget elementor-widget-heading" data-id="8652a2e" data-element_type="widget" data-widget_type="heading.default">
+				<div class="elementor-widget-container">
+			<h2 class="elementor-heading-title elementor-size-default">03.</h2>		</div>
+				</div>
+				<div class="elementor-element elementor-element-e6e34f1 elementor-widget elementor-widget-houzez_elementor_section_title" data-id="e6e34f1" data-element_type="widget" data-widget_type="houzez_elementor_section_title.default">
+				<div class="elementor-widget-container">
+			            <div class="houzez_section_title_wrap section-title-module">
+                                    <h2 class="houzez_section_title"><?php block_field('welcometitle'); ?></h2>
+                
+                            </div>
+            		</div>
+				</div>
+				<div class="elementor-element elementor-element-fbb6ab3 elementor-widget-divider--view-line elementor-widget elementor-widget-divider" data-id="fbb6ab3" data-element_type="widget" data-widget_type="divider.default">
+				<div class="elementor-widget-container">
+					<div class="elementor-divider">
+			<span class="elementor-divider-separator">
+						</span>
+		</div>
+				</div>
+				</div>
+						</div>
+					</div>
+		</div>
+				<div class="elementor-column elementor-col-33 elementor-top-column elementor-element elementor-element-8285f33" data-id="8285f33" data-element_type="column" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+			<div class="elementor-column-wrap elementor-element-populated">
+							<div class="elementor-widget-wrap">
+						<div class="elementor-element elementor-element-9084d9d elementor-widget elementor-widget-houzez_elementor_section_title" data-id="9084d9d" data-element_type="widget" data-widget_type="houzez_elementor_section_title.default">
+				<div class="elementor-widget-container">
+			            <div class="houzez_section_title_wrap section-title-module">
+                                    <h2 class="houzez_section_title">Real Estate Inquiry Form</h2>
+                
+                                    <p class="houzez_section_subtitle">we are waiting to attend to you.</p>
+                            </div>
+            		</div>
+				</div>
+				<div class="elementor-element elementor-element-11a42e0 elementor-button-align-stretch elementor-widget elementor-widget-houzez_elementor_inquiry_form" data-id="11a42e0" data-element_type="widget" data-widget_type="houzez_elementor_inquiry_form.default">
+				<div class="elementor-widget-container">
+			
+        <script type="application/javascript">jQuery(document).bind("ready", function () {
+                houzezValidateElementor("#houzez-form-11a42e0" );
+            });</script>
+
+
+        <form class="elementor-form" id="houzez-form-11a42e0" method="post" name="New Form" action="http://oriadefolahan.me/wp-admin/admin-ajax.php">
+            <input type="hidden" name="form_id" value="11a42e0">
+            <input type="hidden" name="action" value="houzez_ele_inquiry_form">
+            <input type="hidden" name="source_link" value="http://oriadefolahan.me/">
+            <input type="hidden" name="lead_page_id" value="43878">
+            <input type="hidden" name="is_estimation" value="yes">
+            <input type="hidden" name="email_to" value="hello@oriadefolahan.me">
+            <input type="hidden" name="email_subject" value="cozy Havens Enquiry">
+            <input type="hidden" name="email_to_cc" value="">
+            <input type="hidden" name="email_to_bcc" value="">
+            <input type="hidden" name="webhook" value="false">
+            <input type="hidden" name="webhook_url" value="">
+            <input type="hidden" name="redirect_to" value="">
+            <input type="hidden" name="google_recaptcha" value="false">
+
+            <div class="elementor-form-fields-wrapper elementor-labels-above">
+
+                                    <div class="elementor-field-group elementor-column form-group elementor-field-group-60abc23 elementor-col-100">
+                    <label for="form-field-60abc23" class="elementor-field-label">Inquiry type</label>        <div class="elementor-field elementor-select-wrapper">
+            <select name="enquiry_type" id="form-field-60abc23" class="elementor-field-textual form-control elementor-size-sm">
+                <option value="">Select type</option>
+<option value="Purchase">Purchase</option>
+<option value="Rent"> Rent</option>
+<option value="Sell"> Sell</option>
+<option value="Lease"> Lease            </option></select>
+        </div>
+                            </div>
+
+                                        <div class="elementor-field-group elementor-column form-group elementor-field-group-982e10a elementor-col-100">
+                    <label for="form-field-982e10a" class="elementor-field-label">information</label>        <div class="elementor-field elementor-select-wrapper">
+            <select name="user_type" id="form-field-982e10a" class="elementor-field-textual form-control elementor-size-sm">
+                <option value="">I&#039;m an</option>
+<option value="interested buyer">interested buyer</option>
+<option value="interested investor">interested investor</option>            </select>
+        </div>
+                            </div>
+
+                                        <div class="elementor-field-group elementor-column form-group elementor-field-group-9cfec29 elementor-col-50">
+                    <input type="text" name="first_name" id="form-field-9cfec29" class="elementor-field form-control elementor-size-sm elementor-field-textual" placeholder="First Name" title="* First Name">                    </div>
+
+                                        <div class="elementor-field-group elementor-column form-group elementor-field-group-0bc8b7d elementor-col-50">
+                    <input type="text" name="last_name" id="form-field-0bc8b7d" class="elementor-field form-control elementor-size-sm elementor-field-textual" placeholder="Last Name" title="* Last Name">                    </div>
+
+                                        <div class="elementor-field-group elementor-column form-group elementor-field-group-a41bb7a elementor-col-100">
+                    <input type="email" name="email" id="form-field-a41bb7a" class="elementor-field form-control elementor-size-sm elementor-field-textual" placeholder="Email Address" title="* Email Address">                    </div>
+
+                                        <div class="elementor-field-group elementor-column form-group elementor-field-group-6fbe96e elementor-col-100">
+                            <div class="elementor-field elementor-select-wrapper">
+            <select name="e_meta[state]" id="form-field-6fbe96e" class="elementor-field-textual form-control elementor-size-sm">
+                <option value="">Location</option>
+<option data-ref="lagos" data-belong="nigeria" value="lagos"> Lagos state</option>
+<option data-ref="ogun-state" data-belong="nigeria" value="ogun-state"> Ogun State</option>
+<option data-ref="oyo-state" data-belong="nigeria" value="oyo-state"> Oyo State</option>            </select>
+        </div>
+                            </div>
+
+                                        <div class="elementor-field-group elementor-column form-group elementor-field-group-b25f810 elementor-col-100">
+                    <label for="form-field-b25f810" class="elementor-field-label">Property</label>        <div class="elementor-field elementor-select-wrapper">
+            <select name="e_meta[property_type]" id="form-field-b25f810" class="elementor-field-textual form-control elementor-size-sm">
+                <option value="">Select Type</option>
+<option value="apartments"> Apartments</option>
+<option value="commercial"> Commercial</option>
+<option value="commercial-land">-  Commercial Land</option>
+<option value="farm">-  Farm</option>
+<option value="shop">-  Shop</option>
+<option value="warehouse">-  WareHouse</option>
+<option value="commercial-lease"> Commercial Lease</option>
+<option value="commercial-sale"> Commercial Sale</option>
+<option value="fully-detached-with-bq"> Fully detached with BQ</option>
+<option value="office"> Office</option>
+<option value="ready-to-move-in"> ready to move in</option>
+<option value="residential"> Residential</option>
+<option value="detached-bungalow">-  Detached Bungalow</option>
+<option value="flat-apartment">-  Flats &amp; Apartments</option>
+<option value="land">-  Land</option>
+<option value="mini-flats">-  Mini flats</option>
+<option value="self-contain">-  Self Contain</option>
+<option value="semi-detached-bungalow">-  Semi Detached Bungalow</option>
+<option value="semi-detached-duplex">-  Semi-Detached Duplex</option>
+<option value="terraced-bungalow">-  Terraced Bungalow</option>
+<option value="terraced-duplex">-  Terraced Duplex</option>
+<option value="residential-income"> Residential Income</option>
+<option value="residential-land"> Residential Land</option>
+<option value="residential-lease"> Residential Lease</option>
+<option value="town-house"> Town house</option>
+<option value="town-house-with-bq"> Town House with BQ</option>            </select>
+        </div>
+                            </div>
+
+                                        <div class="elementor-field-group elementor-column form-group elementor-field-group-90b27ec elementor-col-100">
+                    <input type="number" name="e_meta[max-price]" id="form-field-90b27ec" class="elementor-field form-control elementor-size-sm elementor-field-textual" placeholder="Max price" title="* Max price" min="0">                    </div>
+
+                    
+                    
+                    
+                    <div class="elementor-field-group elementor-column elementor-field-type-submit elementor-col-100">
+                    <button type="submit" class="btn houzez-submit-button houzez-contact-form-js elementor-button elementor-size-sm elementor-animation-shrink">
+                        <i class="btn-loader houzez-loader-js"></i>
+                        <?php block_field('button-field'); ?>                                            </button>
+                </div>
+            </div>
+<!-- End wrapper-->
+            <br>
+            <div class="ele-form-messages"></div>
+            <div class="error-container"></div>
+
+        </form>
+
+    		</div>
+				</div>
+						</div>
+					</div>
+		</div>
+								</div>
+					</div>
+		</section>
