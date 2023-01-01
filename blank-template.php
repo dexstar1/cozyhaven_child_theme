7910fb9ebeb1005c078f9bff033f06ce36d194ac
dexstar1 <jonsaix852@gmail.com>
2023-01-01 17:08:25 +0100

    added houzes wrapper element to blank template

--- blank-template.php
+++ blank-template.php
@@ -5,15 +5,32 @@
 */
 
 get_header();
+?>
+<section class="page-wrap">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-12 bt-content-wrap">                      
+                <div class="article-wrap">
+                    <?php
+                    // Start the loop.
+                    while ( have_posts() ) : the_post();
 
-if ( have_posts() ) :
-    while ( have_posts() ) :
-        the_post();
+                        // Include the page content template.
+                        get_template_part( 'content', 'page' );
 
-        the_content();
+                        // If comments are open or we have at least one comment, load up the comment template.
+                        if ( comments_open() || get_comments_number() ) :
+                            comments_template();
+                        endif;
 
-    endwhile;
-endif;
-
+                        // End the loop.
+                    endwhile;
+                    ?>
+                </div>
+            </div><!-- bt-content-wrap -->
+        </div><!-- row -->
+    </div><!-- container -->
+</section><!-- listing-wrap -->
+<?php
 get_footer();
 
